b0e53e96800f930e61000a7525808e74df35de12
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2024-09-29 16:42:00 -0500

    feature/added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.scss";
 import Body from "@/components/Body";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Jake Eckhardt",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Body
           children={children}
         />
+        <Analytics />
       </body>
     </html>
   );
