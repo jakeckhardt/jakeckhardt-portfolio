1d4051408ce3f6a765acd00d195335e690b68c97
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2025-06-20 03:25:12 -0500

    fix/removed title

--- src/components/MastheadTitle/MastheadTitles.tsx
+++ src/components/MastheadTitle/MastheadTitles.tsx
@@ -12,7 +12,6 @@ export default function MastheadTitles() {
         "coffee drinker",
         "nerd",
         "fun guy",
-        "Dungeon Master",
     ];
 
     const [moveup, setMoveup] = useState(false);
